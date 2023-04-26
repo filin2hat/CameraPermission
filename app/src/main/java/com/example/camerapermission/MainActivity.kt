8f712e7f1b690834481fe52841d30c1191488350
Biryulin Dmitry <filin2hat@yandex.ru>
2023-04-26 16:35:47 +0300

    saved photos from CAMERA in EXTERNAL STORAGE

--- app/src/main/java/com/example/camerapermission/MainActivity.kt
+++ app/src/main/java/com/example/camerapermission/MainActivity.kt
@@ -1,39 +1,138 @@
 package com.example.camerapermission
 
 import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.camerapermission.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.Executor
+
+private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss"
 
 class MainActivity : AppCompatActivity() {
+    private var imageCapture: ImageCapture? = null
+    private lateinit var executor: Executor
+    private lateinit var binding: ActivityMainBinding
+
+    private val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+        .format(System.currentTimeMillis())
+
     private val launcher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            Toast.makeText(
-                applicationContext, if (isGranted) "Permission is $isGranted" else
-                    "Permission is $isGranted", Toast.LENGTH_SHORT
-            ).show()
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
+            if (map.values.all { it }) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    this, "Permission Denied", Toast.LENGTH_SHORT
+                ).show()
+            }
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        executor = ContextCompat.getMainExecutor(applicationContext)
+
+        binding.takePhotoButton.setOnClickListener { takePhoto() }
+
         checkPermissions()
     }
 
-    private fun checkPermissions() {
-        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)
-//            ContextCompat.checkSelfPermission(
-//                applicationContext,
-//                Manifest.permission.CAMERA
-//            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            Toast.makeText(
-                applicationContext, "Permission is granted", Toast.LENGTH_SHORT
-            ).show()
-        } else {
-            launcher.launch(Manifest.permission.CAMERA)
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, "$name.jpg")
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
         }
 
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        ).build()
+
+        imageCapture.takePicture(
+            outputOptions,
+            executor,
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Photo saved to ${outputFileResults.savedUri}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Photo failed ${exception.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    exception.printStackTrace()
+                }
+            }
+        )
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(applicationContext)
+        cameraProviderFuture.addListener({
+
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build()
+            preview.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+            imageCapture = ImageCapture.Builder().build()
+
+            cameraProvider.unbindAll()
+
+            cameraProvider.bindToLifecycle(
+                this,
+                CameraSelector.DEFAULT_BACK_CAMERA,
+                preview,
+                imageCapture
+            )
+        }, executor)
+    }
+
+    private fun checkPermissions() {
+        val isAllGranted = REQUEST_PERMISSIONS.all { permissions ->
+            ContextCompat.checkSelfPermission(
+                applicationContext, permissions
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+        if (isAllGranted) {
+            startCamera()
+            Toast.makeText(applicationContext, "Permission is granted", Toast.LENGTH_SHORT).show()
+        } else {
+            launcher.launch(REQUEST_PERMISSIONS)
+        }
+    }
+
+    companion object {
+        private val REQUEST_PERMISSIONS: Array<String> = buildList {
+            add(Manifest.permission.CAMERA)
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                add(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
     }
 }
